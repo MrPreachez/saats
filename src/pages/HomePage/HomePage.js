19c0b4c4a7289045c755bc5ea507b09b980545fd
Damon  Chouinard <fengchoui@gmail.com>
2023-05-04 14:58:17 -0700

    home page layout, feature, text and animation

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import { useSpring, useInView, animated as a } from "@react-spring/web";
 import { Link } from "react-router-dom";
 import { forwardRef } from "react";
 
-const HomePage = forwardRef(({}, ref2) => {
+const HomePage = forwardRef(({}, ref4) => {
   const spring1 = useSpring({
     from: { opacity: 0, x: 200 },
     to: { opacity: 1, x: 0 },
@@ -14,12 +14,23 @@ const HomePage = forwardRef(({}, ref2) => {
     to: { opacity: 1, x: 0 },
     config: { mass: 4, friction: 100, tension: 200 },
   });
-  const spring3 = useSpring({
-    from: { opacity: 0, y: 200 },
-    to: { opacity: 1, y: 0 },
-    config: { mass: 5, friction: 100, tension: 200 },
-  });
-  const [ref, springs4] = useInView(() => ({
+  const [ref1, springsA] = useInView(() => ({
+    from: {
+      opacity: 0,
+      x: 300,
+    },
+    to: {
+      opacity: 1,
+      x: 0,
+      config: {
+        mass: 2,
+        friction: 60,
+        tension: 150,
+      },
+    },
+  }));
+
+  const [ref2, springsB] = useInView(() => ({
     from: {
       opacity: 0,
       x: -200,
@@ -34,7 +45,7 @@ const HomePage = forwardRef(({}, ref2) => {
       },
     },
   }));
-  const [ref1, springs5] = useInView(() => ({
+  const [ref3, springsC] = useInView(() => ({
     from: {
       opacity: 0,
       y: 200,
@@ -62,60 +73,56 @@ const HomePage = forwardRef(({}, ref2) => {
               Trail Society
             </a.h2>
           </div>
-          <div className="head__description-position">
-            <h3 className="head__description-title">Our Mission</h3>
-            <a.p style={spring3} className="head__description">
-              <p>
-                • Provide a sustainable network of recreational trails for a
-                diverse range of users
-              </p>
-              <p>• Help preserve public access to public land</p>
-              <p>
-                • Respect and recognition for First Nations' and wildlife
-                interest
-              </p>
-              <p>
-                • Partnership and Collaboration with other user groups and
-                members of the public (ie. land owners, tenure holders, licensee
-                holders)
-              </p>
-              <p>• Preserve the culture heritage of the area</p>
-              <p>• Secure recreation opportunities for all trail users</p>
-              <p>• Provide educational opportunities for the public</p>
-              <p>
-                • Empower youth to work on the trail building and related
-                community service projects
-              </p>{" "}
-            </a.p>
-          </div>
         </div>
       </section>
 
-      <section className="projects__body" ref={ref2} id="projects">
-        <h2 className="projects__title">
-          <span className="projects__feature" />
-          Projects
-          <span className="projects__feature" />
-        </h2>
-      </section>
-      <section className="about__body">
-        <h2 className="about__title">
-          <span className="about__feature" />
+      <section className="mission__body" ref={ref4} id="mission">
+        <h2 className="mission__title">
+          <span className="mission__feature" />
           About
-          <span className="about__feature" />
+          <span className="mission__feature" />
         </h2>
-        <div className="about__container--wrapper">
-          <a.p ref={ref} style={springs4} className="about__text">
+        <h3 className="mission__description-title">Our Mission</h3>
+        <a.p ref={ref1} style={springsA} className="mission__description">
+          <p>
+            • Provide a sustainable network of recreational trails for a diverse
+            range of users
+          </p>
+          <p>• Help preserve public access to public land</p>
+          <p>
+            • Respect and recognition for First Nations' and wildlife interest
+          </p>
+          <p>
+            • Partnership and Collaboration with other user groups and members
+            of the public (ie. land owners, tenure holders, licensee holders)
+          </p>
+          <p>• Preserve the culture heritage of the area</p>
+          <p>• Secure recreation opportunities for all trail users</p>
+          <p>• Provide educational opportunities for the public</p>
+          <p>
+            • Empower youth to work on the trail building and related community
+            service mission
+          </p>{" "}
+        </a.p>
+      </section>
+      <section className="current__body">
+        <h2 className="current__title">
+          <span className="current__feature" />
+          current
+          <span className="current__feature" />
+        </h2>
+        <div className="current__container--wrapper">
+          <a.p ref={ref2} style={springsB} className="current__text">
             Born and raised in BC, I grew up just outside of Vancouver. I
             currently live, work and play in the Socan Valley, just outside of
-            Nelson...<Link to="/about">more</Link>
+            Nelson...<Link to="/current">more</Link>
           </a.p>
-          <div className="about__imgContainer">
+          <div className="current__imgContainer">
             <a.img
-              ref={ref1}
-              style={springs5}
+              ref={ref3}
+              style={springsC}
               src={""}
-              className="about__imgPhoto"
+              className="current__imgPhoto"
               alt="alt-text"
             />
           </div>
