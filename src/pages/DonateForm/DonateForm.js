1a7ccc9e5be5c6c9aea80b3378f5186647d578c7
Damon  Chouinard <fengchoui@gmail.com>
2023-06-07 23:22:46 -0700

    final formatting and styling to donateform

--- src/pages/DonateForm/DonateForm.js
+++ src/pages/DonateForm/DonateForm.js
@@ -29,37 +29,47 @@ const DonateForm = forwardRef(({ donateFormTopRef }, ref) => {
     <main className="donateform__section" ref={donateFormTopRef}>
       <h2 className="donateform__title">Make a Donation to SATS</h2>
       <h3 className="donateform__subheading">
-        Your donation makes a difference in our mission to maintain and build
-        trails in the Slocan area!
+        "Your donation makes a difference in our mission to maintain and build
+        trails in the Slocan area!"
       </h3>
       <p className="donateform__text1">
-        To make a donation the Slocan and Area Trail Society, please first send
-        your donation via etransfer from your institution to:
-        slocanandareatrailsociety@gmail.com
+        To make a donation the Slocan and Area Trail Society, please{" "}
+        <b>first</b> send your donation via etransfer from your institution to:
+        <span className="donateform__mailto">
+          {" "}
+          slocanandareatrailsociety@gmail.com
+        </span>
       </p>
       <p className="donateform__text2">
-        Once your transfer is sent, please fill out the form below and include
-        the 'Additional Details' in the message section. Please be sure to
-        indicate if you would like a receipt and we will issue one via the means
-        you specify. We will confirm with you once we receive your donation (may
-        take up to a week).
+        Once your transfer is sent, please fill out the form below and{" "}
+        <b>include </b>
+        the <b>'Additional Details'</b> in the message section. Please be sure
+        to indicate if you would like a receipt and we will issue one via the
+        means you specify. We will confirm with you once we receive your
+        donation (may take up to a week).
       </p>
       <p className="donateform__text3">
-        Please copy and paste these details into the message section and
+        Please <b>copy and paste</b> these details into the message section and
         complete the details.
       </p>
-      <ul className="donateform__addDetails">
-        <h3 className="donateform__addDetails-head">Additional Details</h3>
-        <li className="donateform__item">Donation Value:</li>
-        <li className="donateform__item">Require Receipt:</li>
-        <li className="donateform__item">Receipt Method - email/physical:</li>
-        <li className="donateform__item">Send receipt to:</li>
-        <li className="donateform__item">
-          Transfer was sent from which institution?
-        </li>
-        <li className="donateform__item">Transfer Id:</li>
-        <li className="donateform__item">Any other details:</li>
-      </ul>
+      <div className="donateform__addDetails--wrapper">
+        <div>
+          <ul className="donateform__addDetails">
+            <h3 className="donateform__addDetails-head">Additional Details</h3>
+            <li className="donateform__item">Donation Value:</li>
+            <li className="donateform__item">Require Receipt:</li>
+            <li className="donateform__item">
+              Receipt Method - email/physical:
+            </li>
+            <li className="donateform__item">Send receipt to:</li>
+            <li className="donateform__item">
+              Transfer was sent from which institution?
+            </li>
+            <li className="donateform__item">Transfer Id:</li>
+            <li className="donateform__item">Any other details:</li>
+          </ul>
+        </div>
+      </div>
 
       <form ref={form} onSubmit={sendEmail} className="donateform__form">
         <div className="donateform__subject--wrapper">
